d1abd464cdbdb109e5d16d1b8629b09d6312afd8
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2023-07-04 14:56:29 +0530

    basic setup for api routes

--- dummy/pages/index.js
+++ dummy/pages/index.js
@@ -1,16 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+function HomePage() {
   return (
     <>
-     <h1>Hello</h1>
+     <h1>HomePage</h1>
     </>
   )
 }
+
+export default HomePage
 
 
